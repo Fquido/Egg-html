8f2d618ed6873d6a58265b8a6075b6fb61c8cd04
Fquido <126190648+Fquido@users.noreply.github.com>
2025-07-18 16:24:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public')); // folder z plikami statycznymi
+app.use(express.static('public'));
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
