2cf03b7c24758489a7d216c42f807d581ab88383
Fquido <126190648+Fquido@users.noreply.github.com>
2025-07-19 02:20:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.SERVER_PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Listening on port ${port}`);
 });
